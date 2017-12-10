9e606ba8386149d6a8de3f271cf9866f1f196cd1
wblt <940422068@qq.com>
2017-12-10 17:41:56 +0800

    d

---
+++ app/src/main/java/com/dev/module/home/ui/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.dev.module.home.ui;
+
+import com.dev.base.R;
+import com.dev.base.view.activity.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/12/10 0010.
+ */
+
+public class HomeActivity extends BaseActivity{
+    @Override
+    protected void setContentLayout() {
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void obtainData() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+}
