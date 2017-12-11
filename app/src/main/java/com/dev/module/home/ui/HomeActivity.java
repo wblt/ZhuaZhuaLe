7a7d380a2bfe03cff26296fc95041cb866dd891e
q454216935 <454216935@qq.com>
2017-12-11 17:59:48 +0800

    自定义的下拉刷新

--- app/src/main/java/com/dev/module/home/ui/HomeActivity.java
+++ app/src/main/java/com/dev/module/home/ui/HomeActivity.java
@@ -1,21 +1,43 @@
 package com.dev.module.home.ui;
 
 import com.dev.base.R;
+import com.dev.base.view.RefreshableView;
 import com.dev.base.view.activity.base.BaseActivity;
+
+import butterknife.BindView;
 
 /**
  * Created by 丁琪 on 2017/12/10 0010.
  */
 
-public class HomeActivity extends BaseActivity{
+public class HomeActivity extends BaseActivity {
+
+//       @BindView(R.id.rfv_home)
+    RefreshableView rfv_home;
+
     @Override
     protected void setContentLayout() {
         setContentView(R.layout.activity_home);
     }
 
+
+
     @Override
     protected void initView() {
-    // 测试能不能提交
+        rfv_home = (RefreshableView) findViewById(R.id.rfv_home);
+        rfv_home.addPullToRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                rfv_home.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        rfv_home.complete();
+                    }
+                }, 2000);
+            }
+        });
+
+
     }
 
     @Override
