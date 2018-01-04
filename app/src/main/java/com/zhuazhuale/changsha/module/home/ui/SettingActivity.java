ba59f2821b282d4dafaef8fd4d2ae73cb377b15f
q454216935 <454216935@qq.com>
2018-01-04 17:05:55 +0800

    改了些布局

--- app/src/main/java/com/zhuazhuale/changsha/module/home/ui/SettingActivity.java
+++ app/src/main/java/com/zhuazhuale/changsha/module/home/ui/SettingActivity.java
@@ -180,10 +180,21 @@ public class SettingActivity extends AppBaseActivity implements View.OnClickList
                 startActivity(intent);
                 break;
             case R.id.ic_setting_bjyx:
-
+                boolean Is_bjyx = PreferenceUtil.getBoolean(getContext(), BaseConstants.Is_bjyx, true);
+                if (Is_bjyx) {
+                    PreferenceUtil.putBoolean(getContext(), BaseConstants.Is_bjyx, false);
+                } else {
+                    PreferenceUtil.putBoolean(getContext(), BaseConstants.Is_bjyx, true);
+                }
 
                 break;
             case R.id.ic_setting_yy:
+                boolean Is_yy = PreferenceUtil.getBoolean(getContext(), BaseConstants.Is_yy, true);
+                if (Is_yy) {
+                    PreferenceUtil.putBoolean(getContext(), BaseConstants.Is_yy, false);
+                } else {
+                    PreferenceUtil.putBoolean(getContext(), BaseConstants.Is_yy, true);
+                }
                 break;
             case R.id.ic_setting_lp:
                 break;
